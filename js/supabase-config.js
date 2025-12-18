69d8be45a89d31fd767493b1cae20d948557bb11
BiellSouza16 <gabrielsouza3740@gmail.com>
2025-12-18 17:25:08 -0300

    supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,7 @@
-// Configuração do Supabase
-const supabaseUrl = 'https://vlnpsieyfiwnaxniqpma.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbnBzaWV5Zml3bmF4bmlxcG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNzYxNzEsImV4cCI6MjA2OTg1MjE3MX0.2Aa6tIrQrGsleDy5UeoAVYBdKXrWpBMyWZoy_RAJaKE';
+import { createClient } from
+  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/+esm';
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  'https://vlnpsieyfiwnaxniqpma.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbnBzaWV5Zml3bmF4bmlxcG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNzYxNzEsImV4cCI6MjA2OTg1MjE3MX0.2Aa6tIrQrGsleDy5UeoAVYBdKXrWpBMyWZoy_RAJaKE'
+);
